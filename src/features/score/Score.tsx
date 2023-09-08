8d7a718d88ec97134edd9ac600fb93f6f4af2ca7
GregV3 <grzegorz.muszynski77@gmail.com>
2023-09-08 15:21:05 +0200

    switching from js to ts completed

--- src/features/score/Score.tsx
+++ src/features/score/Score.tsx
@@ -7,6 +7,6 @@ export const Score = () => {
 
   return (
     // Displaying quantity of matched cards
-    <div className="score-container">Matched: {cardsMatched.length}</div>
+    <div className={cardsMatched.length === 12 ? "score-container score-container-winning": "score-container"}>Matched: {cardsMatched.length}{cardsMatched.length === 12 && " - You win!"}</div>
   );
 };
